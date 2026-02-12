6a18c7c2b4bdebefa7ec9252eedc9a5578c637aa
Julia Marushchenko <marushchenko.yulia@gmail.com>
2026-02-13 00:01:45 +0100

    Typo

--- src/com/mycollections/Main.java
+++ src/com/mycollections/Main.java
@@ -1,5 +1,5 @@
 /**
- *  Java program to demonstrate LinkedHashSet.
+ *  Java program to demonstrate LinkedHashMap.
  */
 
 package com.mycollections;
@@ -16,28 +16,28 @@ public class Main {
     public static void main(String[] args) {
 
         // Creating an empty linked list.
-        LinkedHashMap<Integer, Integer> myLinkedList = new LinkedHashMap<>();
+        LinkedHashMap<Integer, Integer> myLinkedMap = new LinkedHashMap<>();
 
         // Initializing the map.
-        myLinkedList.put(2, 3);
-        myLinkedList.put(3, 4);
-        myLinkedList.put(4, 5);
-        myLinkedList.put(5, 6);
+        myLinkedMap.put(2, 3);
+        myLinkedMap.put(3, 4);
+        myLinkedMap.put(4, 5);
+        myLinkedMap.put(5, 6);
 
         // Printing the map to console. Notice: elements print in order they are added.
-        System.out.println(myLinkedList); // Output: {2=3, 3=4, 4=5, 5=6}
+        System.out.println(myLinkedMap); // Output: {2=3, 3=4, 4=5, 5=6}
 
         // Removing element with key 4.
-        myLinkedList.remove(4);
+        myLinkedMap.remove(4);
 
         // Printing the map to console.
-        System.out.println(myLinkedList); // Output: {2=3, 3=4, 5=6}
+        System.out.println(myLinkedMap); // Output: {2=3, 3=4, 5=6}
 
-        // Removing all elements from myLinkedList.
-        myLinkedList.clear();
+        // Removing all elements from myLinkedMap.
+        myLinkedMap.clear();
 
-        // Printing size of myLinkedList.
-        System.out.println(myLinkedList.size()); // Output: 0.
+        // Printing size of myLinkedMap.
+        System.out.println(myLinkedMap.size()); // Output: 0.
 
     }
 }
